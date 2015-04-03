8131ea25fc4c3412baaef6295efa11bde55e3f25
Michael Sacks <michaelsacks@gmail.com>
2015-04-03 09:58:34 -0700

    Initial Commit

---
+++ RandomItems/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Michael Sacks on 4/3/15.
+//  Copyright (c) 2015 Michael Sacks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
